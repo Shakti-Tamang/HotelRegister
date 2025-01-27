b4c5c86c7f0da0a1b5247d6b418a53296e2fa5e0
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-01-27 20:16:23 +0545

    services added in eruka server

--- servcieregistry/src/main/java/com/nextstep/microservice/ServiceApp.java
+++ servcieregistry/src/main/java/com/nextstep/microservice/ServiceApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer  // Enable Eureka Server
 public class ServiceApp {
 
     public static void main(String[] args) {
--- userservices/src/main/java/com/microservices/App.java
+++ userservices/src/main/java/com/microservices/App.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
 @SpringBootApplication
 @EnableEurekaClient  // Enable Eureka client registration
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class,args);
+        SpringApplication.run(App.class, args);
     }
 }
