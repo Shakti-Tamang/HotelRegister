68d96eb563f91c19a67f34edf062db2c53f5f36c
Shakti-Tamang <sushrabshaktiamu@gmail.com>
2025-03-17 17:06:10 +0545

    created api resposne class

--- aync/src/main/java/com/microservices/asyncthread/controller/ThreadControllers.java
+++ aync/src/main/java/com/microservices/asyncthread/controller/ThreadControllers.java
@@ -1,9 +1,27 @@
 package com.microservices.asyncthread.controller;
 
+import com.microservices.asyncthread.response.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:8016")
 public class ThreadControllers {
+    private final Logger logger;
+
+    public ThreadControllers(){
+        this.logger= LoggerFactory.getLogger(ThreadControllers.class);
+    }
+
+    @PostMapping("/Thread1")
+    public ResponseEntity<ApiResponse>saveThread(){
+        ApiResponse apiResponse=ApiResponse.builder().message("thread started").statusCode(HttpStatus.OK.value()).build();
+        return ResponseEntity.status(HttpStatus.OK).body(apiResponse);
+    }
+
 }
